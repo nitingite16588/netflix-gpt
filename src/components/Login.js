d9de1ba6c1f15356d6ae5c6e97e62a29aedb91f7
nitingite16588 <nitinresume16588@gmail.com>
2024-07-03 19:36:39 +0530

    test

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,13 +4,17 @@ import Header from "./Header"
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true)
 
+
+
+
+  
   const toggleForm = () => {
     setIsSignInForm(!isSignInForm)
   }
   return (
     <>
       <div className="absolute">
-        <Header />
+                  <Header />
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/ff5587c5-1052-47cf-974b-a97e3b4f0656/065df910-dec3-46ae-afa8-7ad2b52dce40/IN-en-20240506-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="background"
